7ebba7a787098680a931928f9ea1991b8dc0bde7
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 18:01:06 +0000

    conversion of data types

---
+++ 01_basis/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score="44"
+let score1="44abc"
+console.log(typeof score);
+console.log(typeof(score));
+let val=Number(score);
+console.log(typeof(val));
+let value=Number(score1)
+console.log(typeof(value));
+console.log(value)
+
+// "44"=> 44
+// "44abc"=> NaN :Not a Number
+// NUll=>0
+// undefined=> NaN: Not a Number
+let islogged="ab"
+let booleanisLoggedIn=Boolean(islogged)
+console.log(booleanisLoggedIn)
+// true=> 1
+// false=> 0
+// ""=> false
+// "abc"=> true
