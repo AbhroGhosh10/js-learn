079ed9367f6659081adc7fe3587e42f7b53edf19
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 14:41:01 +0000

    variables

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=12234
+let accountEmail="abhro@google.com"
+var accountpassword="1234"
+accountCity="jaipur"
+
+// accountId=2
+console.log(accountId)
+console.table([accountId,accountEmail,accountpassword,accountCity])
