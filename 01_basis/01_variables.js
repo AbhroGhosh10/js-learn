798fed128ba75062933fd81d04415b6c87496412
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 15:00:24 +0000

    variables

--- 01_basis/01_variables.js
+++ 01_basis/01_variables.js
@@ -1,8 +1,11 @@
 const accountId=12234
 let accountEmail="abhro@google.com"
 var accountpassword="1234"
+// prefer not to use var
+// because of issue of block scope and functional scope
+let accountState;
 accountCity="jaipur"
 
 // accountId=2
 console.log(accountId)
-console.table([accountId,accountEmail,accountpassword,accountCity])
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
