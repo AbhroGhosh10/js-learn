83c8b6d2a0cef9d7803057d1882e726289e46a2f
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 18:18:38 +0000

    operations

---
+++ 01_basis/04_operations.js
@@ -0,0 +1,17 @@
+let val=3
+let negval=-val;
+// console.log(negval)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
