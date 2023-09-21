fee9e7674850ad55536e944c860c31576654d066
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 17:40:19 +0000

    datatypes

---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all Js code as newer version
+// alert(3+3)//we are using node js not browser
+console.log(3+3)
+console.log("Abhro") 
+
+let name="Abhro";
+let age=22;
+let isloggin=true;
+console.log(typeof(age))
+
+// number=> 2 to power 53 
+// bigint
+// string=>""
+// boolean=>true/false
+// null=> standalone value
+// undefined=> value not assingned
+// symbol=> unique
+
+// object
