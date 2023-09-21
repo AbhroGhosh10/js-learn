68e8ab3434770562ca4e379a149f78bd335c3184
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-21 19:30:28 +0000

    data types-summary

---
+++ 01_basis/datatype-summary.js
@@ -0,0 +1,20 @@
+// tpyes of data types
+// 1. Primitive: 
+// string,symbol,number,null,boolean,BigInt,undefined
+const num=22
+const isloggedIn=true
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+// 2. Non Primitive(reference):
+// Array,Objects,functions
+const heros=["IronMan","spiderman","captianAmerica"]
+
+let myobj={
+    name:"messi",
+    age:36
+
+}
+const myFunction=function(){
+    console.log("hello")
+}
