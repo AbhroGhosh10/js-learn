16914ae04468f0de78454b38407c85a5468d7ab7
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-22 08:04:21 +0000

    strings

---
+++ 01_basis/05_strings.js
@@ -0,0 +1,28 @@
+const name="messi";
+const num=10
+console.log(name+num+" GOAT");
+// console.log(`hello name is ${name} and the jersey number is ${num}`);
+
+const gameName=new String("Abhroghosh")
+// console.log(gameName[0])
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase())
+// console.log(gameName) 
+// console.log(gameName.charAt(2))
+// // console.log(gameName.indexOf('h'));
+// // const newName=gameName.substring(0,4)
+// // console.log(newName);
+// console.log(gameName.length)
+// const anotherName=gameName.slice(-10,1);
+// console.log(anotherName);
+
+// const nameOne="     abhro      ";
+// console.log(nameOne);
+// console.log(nameOne.trim());
+
+const url="https://abhroghosh.com/abhro%20ghosh";
+console.log(url);
+console.log(url.replace('%20','-'));
+
+const nameTwo="abhro-ghosh-engineer-btech"
+console.log(nameTwo.split('-'));
