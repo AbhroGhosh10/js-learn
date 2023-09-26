e1dbd0df1e5c493623339f3cf3da6aaedc3f6f8f
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-26 07:57:22 +0000

    number_and_maths

---
+++ 01_basis/06_number_maths.js
@@ -0,0 +1,25 @@
+// const score=400;
+// console.log(score);
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otherNumber=123.8955;
+// console.log(otherNumber.toPrecision(3));
+// const num=1000000;
+// console.log(num.toLocaleString('en-IN'));
+
+// *************MATH********************
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.abs(2)); 
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.8))
+// console.log(Math.sqrt(69));
+// console.log(Math.min(4,2,3,1,4,));
+// console.log(Math.max(4,2,3,1,4,));
+console.log(Math.random());
+const min=10;
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
