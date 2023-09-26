5acf91bd26c8f11e994c33ba831e1c0499336084
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-26 19:04:34 +0000

    array_1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+// array
+
+const myArray=[1,2,3,4,5,6];
+const myHeros=["batman","spiderman","ironman"]
+const myArray2=new Array(2,3,4,1,2);
+// console.log(myArray[0]);
+// console.log(myArray2);
+
+// Array methods
+// myArray.push(7);
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
+
+
+// myArray.unshift(9)
+// console.log(myArray);
+// myArray.shift()
+
+
+// console.log(myArray.includes(2))
+// console.log(myArray.indexOf(2))
+
+// const newArr=myArray.join();
+// console.log(newArr)
+// console.log(myArray);
+// console.log(typeof myArray);
+
+// slice and splice
+// console.log("A ",myArray)
+// const myn1=myArray.slice(1,3);
+// console.log(myn1);
+// console.log("B ", myArray)
+
+// const  myn2=myArray.splice(1,3);
+// console.log("C ",myArray);
+// console.log(myn2);
+
