6811af33a3048291de9cb1a7a89f788f6684c09f
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-27 19:34:27 +0000

    04a_object

---
+++ 02_basics/04a_objects.js
@@ -0,0 +1,18 @@
+const course={
+    coursename:"Js in hindi",
+    price:"999",
+    courseInstructor:"abhro"
+}
+// course.courseInstructor
+const {courseInstructor:instructor}=course
+// console.log(courseInstructor)
+console.log(instructor);
+
+
+// API
+// Api is look like this below. 
+// {
+//     "name":"abhro",
+//     "age":22,
+//     "course":"Btech"
+// }
