ff52bd0d7bf181f2263e1028dc7938788b2478b9
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-30 19:55:42 +0000

    one.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -20,6 +20,20 @@ else{
 
 const emptobj={}
 
-if (Object.keys(emptobj)===0) {
-    console.log
+if (Object.keys(emptobj).length===0) {
+    console.log("Object is empty")
 }
+
+
+// Nullish Coalescing Operator (??): null undefined
+let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined ?? 12
+val1=null ?? 14 ?? 16
+console.log(val1)
+
+// Terniary operator
+// condition ? true : false;
+const iceTeaprice=100
+iceTeaprice >=80 ? console.log("more than 80") : console.log("less than 80");
