59d35614eaa97fc8bb1a1f72c245ceef54e98f52
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-09-30 20:11:54 +0000

    while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,13 @@
+
+// let index=0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index=index+2;
+// }
+
+let myArray=["batman","superman","flash"]
+let arr=0;
+while (arr<myArray.length) {
+    console.log(myArray[arr])
+    arr=arr+1;
+}
