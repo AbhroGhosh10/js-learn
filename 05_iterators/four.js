4fd39ddfb7e52e6be35f68d6c3fc05f4f9428799
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-10-01 14:33:58 +0000

    four

--- 05_iterators/four.js
+++ 05_iterators/four.js
@@ -13,5 +13,16 @@ for (const key in myObject) {
 const programming =['js','cpp','pyhton','java','css']
 for (const key in programming) {
     // console.log(key);
-    console.log(programming[key]);
+    // console.log(programming[key]);
 }
+
+const map=new Map()
+map.set('IN','India')
+map.set('USA','United States of America')
+map.set('Fr','France')
+map.set('IN','India')
+
+for (const key in map) {
+    console.log(key);
+} 
+
