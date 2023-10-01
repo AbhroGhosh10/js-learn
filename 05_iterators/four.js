f09d79106d06bc3a8878366ee055f88e2a672ad5
Abhro Ghosh <145645596+AbhroGhosh10@users.noreply.github.com>
2023-10-01 08:02:24 +0000

    four

---
+++ 05_iterators/four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js:'javascript',
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is: ${myObject[key]}`);
+
+}
+
+const programming =['js','cpp','pyhton','java','css']
+for (const key in programming) {
+    // console.log(key);
+    console.log(programming[key]);
+}
